dcebf237ac683dc7f75328a2fa6f9e54426d989c
Diego Aldao <diego_ind@live.com.ar>
2022-09-03 18:54:12 -0300

    añadido productsContext y modificado el path de detalle

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 import "./App.css";
-import jsonData from "./data/productos.json";
-import Inicio from "./components/Inicio";
-import CardDetallada from "./components/CardDetallada";
+import Inicio from "./pages/Inicio";
 import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import Detalle from "./pages/Detalle";
+import { ProductsContextProvider } from "./context/ProductsContext";
 
 function App() {
-  const [data, setData] = useState(jsonData.products);
-  const [producto, setProducto] = useState();
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Inicio data={data} setProducto={setProducto} />}
-      />
-      <Route
-        path="/detalle"
-        element={<CardDetallada data={data} producto={producto} />}
-      />
-    </Routes>
+    <ProductsContextProvider>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/detalle/:id" element={<Detalle />} />
+      </Routes>
+    </ProductsContextProvider>
   );
 }
 
