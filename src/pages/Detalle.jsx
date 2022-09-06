9eab74af63ac8be1ddf5d0c0f51e085b66441589
Diego Aldao <diego_ind@live.com.ar>
2022-09-06 12:17:04 -0300

    renombrados los componentes card

--- src/pages/Detalle.jsx
+++ src/pages/Detalle.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import CardDetallada from "../components/CardDetallada";
+import ProductoDetallado from "../components/ProductoDetallado";
 
 const Detalle = ({ params }) => {
-  return <CardDetallada idProducto={params.id} />;
+  return <ProductoDetallado idProducto={params.id} />;
 };
 
 export default Detalle;
