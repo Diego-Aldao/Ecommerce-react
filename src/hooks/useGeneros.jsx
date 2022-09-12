8b616b776540cc40b26e6858d232b342ce414543
Diego Aldao <diego_ind@live.com.ar>
2022-09-12 19:38:31 -0300

    añadido hook para obtener el contenido de cada genero en especifico

--- src/hooks/useGeneros.jsx
+++ src/hooks/useGeneros.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import JsonData from "../data/categoriasEsp.json";
 
 const useGeneros = () => {
@@ -10,51 +9,6 @@ const useGeneros = () => {
   const contenidoMujer = navigation[1].children.filter(
     (obj) => obj.content.title == "Home" || obj.content.title == "Categories"
   );
-
-  /* console.log(contenidoH);
-
-  const botones = navigation.map((genero) => {
-    return <button key={genero.id}>{genero.content.title}</button>;
-  });
-
-  const homeMovileHombre = navigation[0].children[0].children.map((child) => {
-    return (
-      <Item
-        className="btn-home"
-        key={child.id}
-        background={child.content.mobileImageUrl}
-      >
-        <Link to="/mujer">
-          <Titulo>{child.content.title}</Titulo>
-        </Link>
-      </Item>
-    );
-  });
-
-  const contenidoM = navigation[1].children[4].children.map((categoria) => {
-    return (
-      <Item key={categoria.id} background={categoria.content.mobileImageUrl}>
-        <Link to="/mujer">
-          {categoria.content.subTitle ? (
-            <>
-              <Titulo className="compartido">{categoria.content.title}</Titulo>
-              <Titulo className="compartido">
-                {categoria.content.subTitle}
-              </Titulo>
-            </>
-          ) : (
-            <Titulo>{categoria.content.title}</Titulo>
-          )}
-        </Link>
-      </Item>
-    );
-  });
-/*   useEffect(() => {
-    setContenidoHombre(filterHombre);
-       setBotonesGenero(botones);
-    setHomeHombre(homeMovileHombre);
-    setContenidoMujer(contenidoM);
-  }, [setContenidoHombre]); */
   return { contenidoHombre, contenidoMujer };
 };
 
