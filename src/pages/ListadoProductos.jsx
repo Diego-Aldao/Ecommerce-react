ccc16a16b5683be0a7c381abff4f74a02556bab5
Diego Aldao <diego_ind@live.com.ar>
2022-09-13 20:05:04 -0300

    añadida la pagina listado de productos

--- src/pages/ListadoProductos.jsx
+++ src/pages/ListadoProductos.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import Banner from "../components/ListadoProductos/Banner";
+import Filtro from "../components/ListadoProductos/Filtro/Filtro";
+import Main from "../components/ListadoProductos/Main/Main";
+import Nav from "../components/ListadoProductos/Nav";
+import Layout from "../layout/Layout";
 
 const ListadoProductos = ({ params }) => {
-  console.log(params.genero);
-  return <div>{params.genero}</div>;
+  const { cat2 } = params;
+  console.log(params.querys);
+  const idCategoria = params.querys.replace("categoryId=", "");
+  console.log(idCategoria);
+
+  return (
+    <Layout genero={params.genero}>
+      <Nav params={params} />
+      <Banner categoria={cat2} />
+      <Filtro />
+      <Main idCategoria={idCategoria} />
+    </Layout>
+  );
 };
 
 export default ListadoProductos;
