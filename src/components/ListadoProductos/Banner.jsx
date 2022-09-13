a9bd20b7443ac289be8f066fd528129fa016a674
Diego Aldao <diego_ind@live.com.ar>
2022-09-13 20:03:17 -0300

    agregado el banner del listado de productos

---
+++ src/components/ListadoProductos/Banner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const Contenedor = styled.section`
+  width: 100%;
+  padding: 20px 0px 0px;
+  min-height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f3f3f3;
+
+  h1 {
+    text-align: center;
+    padding: 0px 16px 8px;
+    text-transform: capitalize;
+    font-size: clamp(24px, 32px, 3vw);
+  }
+`;
+
+const Banner = ({ categoria }) => {
+  const nombreCategoria = categoria.replace("-", " ");
+  return (
+    <Contenedor>
+      <h1>{nombreCategoria}</h1>
+    </Contenedor>
+  );
+};
+
+export default Banner;
