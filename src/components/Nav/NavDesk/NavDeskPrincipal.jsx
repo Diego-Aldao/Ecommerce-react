28039e7c98d191fe738d27d191ab2e4d084f5c2b
Diego Aldao <diego_ind@live.com.ar>
2022-09-14 20:59:50 -0300

    renombradas las navegaciones principales desk y movil

---
+++ src/components/Nav/NavDesk/NavDeskPrincipal.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Dropdown from "./Dropdown";
+
+const Navegacion = styled.nav`
+  display: none;
+  width: 100%;
+  height: 50px;
+  padding: 0px 32px;
+  background: #796764;
+  position: relative;
+  button {
+    flex: 1 1 auto;
+    padding: 10px;
+    background: none;
+    color: white;
+    border: none;
+    text-transform: capitalize;
+    font-size: 12px;
+  }
+  @media (min-width: 1024px) {
+    display: flex;
+  }
+`;
+
+const NavDeskPrincipal = ({ contenido }) => {
+  const categorias = contenido[1].children.filter(
+    (obj) => obj.channelExclusions.length !== 1
+  );
+  const [visible, setVisible] = useState(false);
+  const [hijos, setHijos] = useState();
+
+  const handleMouseOver = (children) => {
+    setHijos(children);
+    setVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setVisible(false);
+  };
+
+  const botonesCategorias = categorias.map((categoria) => {
+    return (
+      <React.Fragment key={categoria.id}>
+        <button
+          onMouseOver={() => {
+            handleMouseOver(categoria.children);
+          }}
+          onMouseLeave={handleMouseLeave}
+        >
+          {categoria.content.title}
+        </button>
+        <Dropdown hijos={hijos} visible={visible} setVisible={setVisible} />
+      </React.Fragment>
+    );
+  });
+
+  return (
+    <>
+      <Navegacion>{botonesCategorias}</Navegacion>
+    </>
+  );
+};
+
+export default NavDeskPrincipal;
---
+++ src/components/Nav/NavMovil/NavMovilPrincipal.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import DetalleNavMovil from "./DetalleNavMovil";
+import ListaNavMovil from "./ListaNavMovil";
+import { useLocation } from "wouter";
+import {
+  Contenido,
+  Navegacion,
+  BotonCerrar,
+  ContenedorNav,
+  ContenidoSimple,
+  ContenidoDetallado,
+} from "../../NavMovil";
+
+const ContenedorBotones = styled.div`
+  width: 100%;
+  display: flex;
+  border-bottom: 1px solid #0000002d;
+  position: relative;
+  button {
+    flex: 1 1 auto;
+    height: 50px;
+    background: none;
+    border: none;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-family: "Josefin Sans", sans-serif;
+    letter-spacing: 1px;
+  }
+  &:after {
+    content: "";
+    top: 50%;
+    right: 50%;
+    margin-top: -10px;
+    position: absolute;
+    height: 20px;
+    width: 1px;
+    background-color: #2d2d2d1a;
+  }
+`;
+
+const NavMovilPrincipal = ({ visible, setvisible, contenido, genero }) => {
+  const [categoria, setCategoria] = useState();
+  const [posicion, setPosicion] = useState(false);
+  const [location, setLocation] = useLocation();
+
+  const handleClick = () => {
+    setvisible(!visible);
+  };
+
+  const handleClickGenero = (genero) => {
+    setvisible(!visible);
+    setLocation(`/${genero}`);
+    window.location.reload();
+  };
+
+  return (
+    <Contenido visible={visible} left={true}>
+      <Navegacion visible={visible} left={true}>
+        <ContenedorNav posicion={posicion}>
+          <ContenidoSimple>
+            <ContenedorBotones>
+              <button
+                onClick={() => {
+                  handleClickGenero("mujer");
+                }}
+              >
+                mujer
+              </button>
+              <button
+                onClick={() => {
+                  handleClickGenero("hombre");
+                }}
+              >
+                hombre
+              </button>
+            </ContenedorBotones>
+            <ListaNavMovil
+              contenido={contenido}
+              setPosicion={setPosicion}
+              setCategoria={setCategoria}
+              genero={genero}
+              setvisible={setvisible}
+            />
+          </ContenidoSimple>
+          <ContenidoDetallado>
+            <DetalleNavMovil categoria={categoria} setPosicion={setPosicion} />
+          </ContenidoDetallado>
+        </ContenedorNav>
+      </Navegacion>
+      <BotonCerrar onClick={handleClick}>
+        <span></span>
+        <span></span>
+      </BotonCerrar>
+    </Contenido>
+  );
+};
+
+export default NavMovilPrincipal;
