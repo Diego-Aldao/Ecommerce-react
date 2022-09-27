446b0183c6e2f0f1fc518ec5d8c6d1d6e77814da
Diego Aldao <diego_ind@live.com.ar>
2022-09-27 18:28:06 -0300

    añadido hook para manipular la info del home

---
+++ src/hooks/useContentHome.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import DataHome from "../data/Home.json";
+
+const useContentHome = ({ genero }) => {
+  const data = DataHome;
+  const [gridHome, setGridHome] = useState([]);
+  const [heroHome, setHeroHome] = useState([]);
+  const [descuentoHome, setDescuentoHome] = useState([]);
+  const [promocionHome, setPromocionHome] = useState([]);
+  const [marcasHome, setMarcasHome] = useState([]);
+
+  const currentGenero = genero == "hombre" ? genero : "mujer";
+
+  const contenidoActual = data.home.filter(
+    (data) => data.nombre === currentGenero
+  );
+
+  const hero = contenidoActual[0].children.filter(
+    (child) => child.nombre == "Hero"
+  );
+
+  const gridPrincipal = contenidoActual[0].children.filter(
+    (child) => child.nombre == "GridPrincipal"
+  );
+
+  const descuento = contenidoActual[0].children.filter(
+    (child) => child.nombre == "Descuento"
+  );
+
+  const promocion = contenidoActual[0].children.filter(
+    (child) => child.nombre == "Promocion"
+  );
+
+  const marcas = contenidoActual[0].children.filter(
+    (child) => child.nombre == "Marcas"
+  );
+
+  useEffect(() => {
+    setGridHome(gridPrincipal[0].children);
+    setHeroHome(hero[0]);
+    setDescuentoHome(descuento[0]);
+    setPromocionHome(promocion[0].children);
+    setMarcasHome(marcas[0].children);
+  }, []);
+
+  return { gridHome, heroHome, descuentoHome, promocionHome, marcasHome };
+};
+
+export default useContentHome;
