edec191716dd2799123e42ff2721b61a4959ba96
Diego Aldao <diego_ind@live.com.ar>
2022-09-29 15:51:53 -0300

    añadido otro posible path para el listado de productos y el path para detalle producto

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Detalle from "./pages/Detalle";
 import { ProductsContextProvider } from "./context/ProductsContext";
 import Search from "./pages/Search";
 import ListadoProductos from "./pages/ListadoProductos";
+import DetalleProducto from "./pages/DetalleProducto";
 
 function App() {
   return (
@@ -18,12 +19,15 @@ function App() {
       <Switch>
         <Route
           component={ListadoProductos}
-          path="/:genero/:cat1/:cat2/:cat/:querys"
+          path="/:genero/:cat1/:cat2/cat/:querys"
         />
 
+        <Route component={ListadoProductos} path="/:genero/:cat1/cat/:querys" />
+      </Switch>
+      <Switch>
         <Route
-          component={ListadoProductos}
-          path="/:genero/:cat1/:cat/:querys"
+          component={DetalleProducto}
+          path="/:categoria/:nombre/prd/:querys"
         />
       </Switch>
     </ProductsContextProvider>
