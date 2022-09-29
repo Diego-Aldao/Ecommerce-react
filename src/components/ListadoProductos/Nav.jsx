96b2e59fe3609e83881d71163dc9217677fda8e3
Diego Aldao <diego_ind@live.com.ar>
2022-09-29 15:49:39 -0300

    adaptado el nav para que funcione tanto en el listado de productos como en el detalle

--- src/components/ListadoProductos/Nav.jsx
+++ src/components/ListadoProductos/Nav.jsx
@@ -11,53 +11,90 @@ const Contenedor = styled.nav`
   box-shadow: 1px 1px 5px #00000049;
   display: none;
   border-bottom: 1px solid #cecece;
+  position: relative;
   @media (min-width: 480px) {
     display: flex;
   }
   ul {
     display: flex;
-    flex-wrap: wrap;
-    font-size: 14px;
+    flex-wrap: nowrap;
+    white-space: nowrap;
+    overflow: hidden;
+    position: relative;
     li {
       flex: 0 1 auto;
       display: flex;
       align-items: center;
       height: 50px;
-      text-transform: capitalize;
+      font-size: 14px;
     }
+    li p::first-letter {
+      text-transform: uppercase;
+    }
+
     span {
-      padding: 0px 10px 0px 15px;
+      padding: 0px 7px 0px 10px;
       display: flex;
       align-items: center;
     }
   }
+  ul li:last-of-type {
+    color: #636363;
+  }
+  &:after {
+    position: absolute;
+    width: 100px;
+    height: 100%;
+    content: "";
+    right: 18px;
+    top: 0;
+    background: linear-gradient(to right, #ffffff6a, #ffffff);
+  }
 `;
 
 const Nav = ({ params }) => {
-  const { cat1, genero } = params;
-  const cat2 = params.cat2.replaceAll("-", " ");
+  const prevPath = JSON.parse(localStorage.getItem("path"));
+  const nuevoPath = params.genero
+    ? localStorage.setItem("path", JSON.stringify(params))
+    : null;
+
+  const { cat1, genero, cat2 } = params.genero ? params : prevPath;
+  const { nombre } = params;
+  const nombreFormateado = params.nombre?.replaceAll("-", " ");
+  const cat1Formateada = cat1?.replaceAll("-", " ");
+  const cat2Formateada = cat2?.replaceAll("-", " ");
   return (
     <Contenedor>
       <ul>
-        <li>inicio</li>
         <li>
-          <span>
-            <MdChevronRight></MdChevronRight>
-          </span>
-          {genero}
+          <p>inicio</p>
         </li>
         <li>
           <span>
             <MdChevronRight></MdChevronRight>
           </span>
-          {cat1}
+          <p>{genero}</p>
         </li>
         <li>
           <span>
             <MdChevronRight></MdChevronRight>
           </span>
-          {cat2}
+          <p>{cat1Formateada}</p>
         </li>
+        <li>
+          <span>
+            <MdChevronRight></MdChevronRight>
+          </span>
+          <p>{cat2Formateada}</p>
+        </li>
+        {nombre ? (
+          <li>
+            <span>
+              <MdChevronRight></MdChevronRight>
+            </span>
+            <p>{nombreFormateado}</p>
+          </li>
+        ) : null}
       </ul>
     </Contenedor>
   );
