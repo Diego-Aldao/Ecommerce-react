155cbd6b80f400ba4135114aaef472645d062e7c
Diego Aldao <diego_ind@live.com.ar>
2022-10-01 15:04:49 -0300

    añadido el context provider de los links que se comparten entre la nav y por ahora el inicio

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Footer from "../components/Footer/Footer";
 import Navegacion from "../components/Nav/Nav";
+import { LinksContextProvider } from "../context/LinksContext";
 
 const Layout = ({ children, genero }) => {
   return (
-    <>
+    <LinksContextProvider>
       <Navegacion genero={genero} />
       {children}
       <Footer />
-    </>
+    </LinksContextProvider>
   );
 };
 
