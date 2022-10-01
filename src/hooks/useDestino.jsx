1d411869769035f30b22259bd4c1e337ae94546b
Diego Aldao <diego_ind@live.com.ar>
2022-10-01 15:03:41 -0300

    añadido useState para guardar el link formateado y una validacion dentro del useEffect para que se ejecute solo si el link no es undefined

--- src/hooks/useDestino.jsx
+++ src/hooks/useDestino.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const useDestino = (link) => {
-  let ruta = link.replaceAll("https://www.asos.com/es/", "/").split("?", 2);
-  let url = ruta[0];
-  let querys = ruta[1].replaceAll("cid=", "categoryId=");
-  const destino = `${url}${querys}`;
-  return { destino };
+  const [linkFormateado, setLinkFormateado] = useState("");
+
+  useEffect(() => {
+    if (link.includes("/")) {
+      let ruta = link.replaceAll("https://www.asos.com/es/", "/").split("?", 2);
+      let url = ruta[0];
+      let querys = ruta[1].replaceAll("cid=", "categoryId=");
+      const destino = `${url}${querys}`;
+      setLinkFormateado(destino);
+    }
+  }, [link]);
+  return { linkFormateado };
 };
 
 export default useDestino;
