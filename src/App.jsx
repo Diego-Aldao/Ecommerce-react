365f0b9914471bcb7f09e92a55453c496d68aa46
Diego Aldao <diego_ind@live.com.ar>
2022-11-14 12:27:16 -0300

    añadido el context de los items guardados y en el carrito, y añadidas las rutas del panel de usuario y de la lista de productos guardados

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,42 @@ import Search from "./pages/Search";
 import ListadoProductos from "./pages/ListadoProductos";
 import DetalleProducto from "./pages/DetalleProducto";
 import PanelUsuario from "./pages/PanelUsuario";
+import Guardados from "./pages/Guardados";
+import { GuardadosContextProvider } from "./context/GuardadosContext";
+import { CarritoContextProvider } from "./context/CarritoContext";
 
 function App() {
   return (
     <ProductsContextProvider>
-      <Switch>
-        <Route path="/" component={Inicio} />
-        <Route path="/:genero" component={Inicio} />
-      </Switch>
-      <Route component={Detalle} path="/detalle/:id" />
-      <Route component={Search} path="/search/:keyword" />
-      <Switch>
-        <Route
-          component={ListadoProductos}
-          path="/:genero/:cat1/:cat2/cat/:querys"
-        />
+      <GuardadosContextProvider>
+        <CarritoContextProvider>
+          <Switch>
+            <Route path="/" component={Inicio} />
+            <Route path="/:genero" component={Inicio} />
+          </Switch>
+          <Route component={Detalle} path="/detalle/:id" />
+          <Route component={Search} path="/search/:keyword" />
+          <Route component={PanelUsuario} path="/usuario/panelUsuario" />
+          <Route component={Guardados} path="/usuario/guardados" />
+          <Switch>
+            <Route
+              component={ListadoProductos}
+              path="/:genero/:cat1/:cat2/cat/:querys"
+            />
 
-        <Route component={ListadoProductos} path="/:genero/:cat1/cat/:querys" />
-      </Switch>
-      <Switch>
-        <Route
-          component={DetalleProducto}
-          path="/:categoria/:nombre/prd/:querys"
-        />
-      </Switch>
-      <Route component={PanelUsuario} path="/usuario/panelUsuario" />
+            <Route
+              component={ListadoProductos}
+              path="/:genero/:cat1/cat/:querys"
+            />
+          </Switch>
+          <Switch>
+            <Route
+              component={DetalleProducto}
+              path="/:categoria/:nombre/prd/:querys"
+            />
+          </Switch>
+        </CarritoContextProvider>
+      </GuardadosContextProvider>
     </ProductsContextProvider>
   );
 }
