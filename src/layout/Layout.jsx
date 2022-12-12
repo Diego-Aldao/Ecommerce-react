3cead6d87e114f038b962d3f85755be9ee084204
Diego Aldao <diego_ind@live.com.ar>
2022-12-12 15:04:43 -0300

    añadido un state que contiene la navegacion sacada del json local

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer/Footer";
 import Navegacion from "../components/Nav/Nav";
 import { LinksContextProvider } from "../context/LinksContext";
+import jsonData from "../data/categoriasEsp.json";
 
 const Layout = ({ children, genero }) => {
+  const { navigation } = jsonData;
+  const [navegacion, setNavegacion] = useState(navigation);
+
   return (
     <LinksContextProvider>
-      <Navegacion genero={genero} />
+      <Navegacion genero={genero} navegacion={navegacion} />
       {children}
       <Footer />
     </LinksContextProvider>
