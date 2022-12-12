6cd48787b90544d2f443a81cce4086e356ac2770
Diego Aldao <diego_ind@live.com.ar>
2022-12-12 14:47:28 -0300

    eliminados componentes y hooks de prueba

--- src/pages/Detalle.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProductoDetallado from "../components/ProductoDetallado";
-
-const Detalle = ({ params }) => {
-  return <ProductoDetallado idProducto={params.id} />;
-};
-
-export default Detalle;
--- src/pages/Search.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Producto from "../components/Producto";
-
-const Search = ({ params }) => {
-  return <Producto keyword={params.keyword} />;
-};
-
-export default Search;
